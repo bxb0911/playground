60b2eaaf78fce5bd4475b6b2329a55ea955cc270
bxb0911 <baiyanli@zuoyebang.com>
2020-12-21 10:03:00 +0800

    feat: init

---
+++ demo/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      count: 0,
+      msg: "Hello World~",
+    };
+  },
+};
+</script>
