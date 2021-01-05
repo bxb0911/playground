0f6c500e00f61979a7922aa0eb8dbf033a68fd5f
bxb0911 <baiyanli@zuoyebang.com>
2021-01-05 09:24:49 +0800

    feat: code changes

---
+++ ESM/basic-modules/hahaha.js
@@ -0,0 +1,8 @@
+hahaha();
+
+function hahaha() {
+  console.log('hahaha');
+}
+
+a = 2;
+console.log(a);
--- demo/src/main.js
+++ demo/src/main.js
@@ -2,3 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 
 createApp(App).mount("#app");
+
+// 入口main.js默认采用的是es6原生的模块（import语法在ES6中默认发送一个请求）
+// 默认会把vue的模块增加一个前缀 /@modules
+// 把.vue文件在后端给解析成一个对象了（唯一就是编译了.vue文件）
